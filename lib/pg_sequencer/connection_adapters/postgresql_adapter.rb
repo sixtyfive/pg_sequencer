8135fddbb6d1a9c1760f5cb67937f880d37000bf
Walker Wilkins <wwilkins@gmail.com>
2015-03-17 10:41:11 -0500

    Hack in PR from HellRok

--- lib/pg_sequencer/connection_adapters/postgresql_adapter.rb
+++ lib/pg_sequencer/connection_adapters/postgresql_adapter.rb
@@ -1,22 +1,22 @@
 module PgSequencer
   module ConnectionAdapters
-    
+
     class SequenceDefinition < Struct.new(:name, :options)
     end
-    
+
     module PostgreSQLAdapter
       def create_sequence(name, options = {})
         execute create_sequence_sql(name, options)
       end
-      
+
       def drop_sequence(name)
         execute drop_sequence_sql(name)
       end
-      
+
       def change_sequence(name, options = {})
         execute change_sequence_sql(name, options)
       end
-      
+
       # CREATE [ TEMPORARY | TEMP ] SEQUENCE name [ INCREMENT [ BY ] increment ]
       #     [ MINVALUE minvalue | NO MINVALUE ] [ MAXVALUE maxvalue | NO MAXVALUE ]
       #     [ START [ WITH ] start ] [ CACHE cache ] [ [ NO ] CYCLE ]
@@ -32,17 +32,17 @@ module PgSequencer
         options.delete(:restart)
         "CREATE SEQUENCE #{name}#{sequence_options_sql(options)}"
       end
-      
+
       def drop_sequence_sql(name)
         "DROP SEQUENCE #{name}"
       end
-      
+
       def change_sequence_sql(name, options = {})
         return "" if options.blank?
         options.delete(:start)
         "ALTER SEQUENCE #{name}#{sequence_options_sql(options)}"
       end
-      
+
       def sequence_options_sql(options = {})
         sql = ""
         sql << increment_option_sql(options)  if options[:increment] or options[:increment_by]
@@ -54,7 +54,7 @@ module PgSequencer
         sql << cycle_option_sql(options)
         sql
       end
-      
+
       def sequences
         # sequence_temp=# select * from temp;
         # -[ RECORD 1 ]-+--------------------
@@ -68,13 +68,18 @@ module PgSequencer
         # log_cnt       | 26
         # is_cycled     | f
         # is_called     | t
-        sequence_names = select_all("SELECT c.relname FROM pg_class c WHERE c.relkind = 'S' order by c.relname asc").map { |row| row['relname'] }
-        
+        sequence_names = select_all(<<-SQL).map { |row| row['relname'] }
+          SELECT seq.relname FROM pg_class AS seq
+          JOIN pg_namespace ns ON (seq.relnamespace=ns.oid)
+          WHERE seq.relkind = 'S' AND NOT EXISTS (SELECT * FROM pg_depend WHERE objid=seq.oid AND deptype='a')
+          AND pg_table_is_visible(seq.oid) ORDER BY seq.relname;
+        SQL
+
         all_sequences = []
-        
+
         sequence_names.each do |sequence_name|
           row = select_one("SELECT * FROM #{sequence_name}")
-          
+
           options = {
             :increment => row['increment_by'].to_i,
             :min       => row['min_value'].to_i,
@@ -83,18 +88,18 @@ module PgSequencer
             :cache     => row['cache_value'].to_i,
             :cycle     => row['is_cycled'] == 't'
           }
-          
+
           all_sequences << SequenceDefinition.new(sequence_name, options)
         end
-        
+
         all_sequences
       end
-      
+
       protected
       def increment_option_sql(options = {})
         " INCREMENT BY #{options[:increment] || options[:increment_by]}"
       end
-      
+
       def min_option_sql(options = {})
         case options[:min]
         when nil then ""
@@ -102,7 +107,7 @@ module PgSequencer
         else " MINVALUE #{options[:min]}"
         end
       end
-      
+
       def max_option_sql(options = {})
         case options[:max]
         when nil then ""
@@ -110,19 +115,19 @@ module PgSequencer
         else " MAXVALUE #{options[:max]}"
         end
       end
-      
+
       def restart_option_sql(options = {})
         " RESTART WITH #{options[:restart] || options[:restart_with]}"
       end
-      
+
       def start_option_sql(options = {})
         " START WITH #{options[:start] || options[:start_with]}"
       end
-      
+
       def cache_option_sql(options = {})
         " CACHE #{options[:cache]}"
       end
-      
+
       def cycle_option_sql(options = {})
         case options[:cycle]
         when nil then ""
@@ -130,7 +135,7 @@ module PgSequencer
         else " CYCLE"
         end
       end
-      
+
     end
   end
 end
