8bbafc5bbc23d498988d2a17b76632b1ded9d41c
Jasdeep Gosal <jasdeep.gosal@charitywater.org>
2015-04-20 18:45:47 -0400

    Include PGSequencer stuff in PostgreSQLAdapter
    http://i3.kym-cdn.com/photos/images/facebook/000/234/765/b7e.jpg

--- lib/pg_sequencer/railtie.rb
+++ lib/pg_sequencer/railtie.rb
@@ -1,5 +1,5 @@
   require "pg_sequencer/connection_adapters/postgresql_adapter"
-  
+
   ActiveRecord::Schema.class_eval do
     include PgSequencer::ConnectionAdapters::PostgreSQLAdapter
   end
@@ -9,15 +9,19 @@ module PgSequencer
     initializer "pg_sequencer.load_adapter" do
       ActiveSupport.on_load :active_record do
         require 'pg_sequencer/connection_adapters/postgresql_adapter'
-        
+
         ActiveRecord::ConnectionAdapters.module_eval do
           include PgSequencer::ConnectionAdapters::PostgreSQLAdapter
         end
-      
+
+        ActiveRecord::ConnectionAdapters::PostgreSQLAdapter.class_eval do
+          include PgSequencer::ConnectionAdapters::PostgreSQLAdapter
+        end
+
         ActiveRecord::SchemaDumper.class_eval do
           include PgSequencer::SchemaDumper
         end
-        
+
       end
 
   #     if defined?(ActiveRecord::Migration::CommandRecorder)
@@ -25,10 +29,10 @@ module PgSequencer
   #         include PgSequencer::Migration::CommandRecorder
   #       end
   #     end
-  # 
+  #
   #     # PgSequencer::Adapter.load!
   #   end
-  
+
     end # initializer
   end
 end
