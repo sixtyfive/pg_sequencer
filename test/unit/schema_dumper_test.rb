16427388eae12aa0e9a8318c9007b30885f243d1
Jasdeep Gosal <jasdeep.gosal@charitywater.org>
2015-09-16 20:12:43 -0400

    Updated schema_dumper to include the sequences in dump.final instead of appending it to the passed in stream.
    After upgrading to Rails 4.2.X & doing all the associated gem upgrades, we were no longer able to run bin/rake db:schema:dump because we got this error:
    
    ```
    NoMethodError: private method `puts' called for nil:NilClass
    …/gems/pg_sequencer-8bbafc5bbc23/lib/pg_sequencer/schema_dumper.rb:28:in `sequences'
    ```
    
    After some digging, I was able to determine that that schema_plus_core’s schema_dumper no longer passes around the stream like it used to, and appending to `@dump.final` seemed like the simplest way to recreate the old schema dump file, which always just had the pg_sequencer sequences at the very end.

--- test/unit/schema_dumper_test.rb
+++ test/unit/schema_dumper_test.rb
@@ -21,8 +21,9 @@ class SchemaDumperTest < Test::Unit::TestCase
   end
 
   class MockSchemaDumper
-    def initialize(connection)
+    def initialize(connection, dump)
       @connection = connection
+      @dump = dump
     end
 
     def self.dump(conn, stream)
@@ -63,6 +64,7 @@ class SchemaDumperTest < Test::Unit::TestCase
       }
 
       @stream = MockStream.new
+      @dump = OpenStruct.new(final: "")
     end
 
     should "output all sequences correctly" do
@@ -72,17 +74,20 @@ class SchemaDumperTest < Test::Unit::TestCase
 
       @conn = MockConnection.new(sequences)
 
-      expected_output = <<-SCHEMAEND
+      expected_stream_output = <<-SCHEMAEND
 # Fake Schema Header
 # (No Tables)
-  create_sequence "seq_t_item", :increment => 1, :min => 1, :max => 2000000, :start => 1, :cache => 5, :cycle => true
-  create_sequence "seq_t_user", :increment => 1, :min => 1, :max => 2000000, :start => 1, :cache => 5, :cycle => true
-
 # Fake Schema Trailer
 SCHEMAEND
 
-      MockSchemaDumper.dump(@conn, @stream)
-      assert_equal(expected_output.strip, @stream.to_s)
+      expected_dump_final_output = <<-SCHEMAEND
+  create_sequence "seq_t_item", :increment => 1, :min => 1, :max => 2000000, :start => 1, :cache => 5, :cycle => true
+  create_sequence "seq_t_user", :increment => 1, :min => 1, :max => 2000000, :start => 1, :cache => 5, :cycle => true
+SCHEMAEND
+
+      MockSchemaDumper.new(@conn, @dump).dump(@stream)
+      assert_equal(expected_stream_output.strip, @stream.to_s)
+      assert_equal(expected_dump_final_output.strip, @dump.final.strip)
     end
 
     context "when min specified as false" do
@@ -94,17 +99,20 @@ SCHEMAEND
       end
 
       should "properly quote false values in schema output" do
-        expected_output = <<-SCHEMAEND
+        expected_stream_output = <<-SCHEMAEND
 # Fake Schema Header
 # (No Tables)
-  create_sequence "seq_t_item", :increment => 1, :min => false, :max => 2000000, :start => 1, :cache => 5, :cycle => true
-  create_sequence "seq_t_user", :increment => 1, :min => false, :max => 2000000, :start => 1, :cache => 5, :cycle => true
-
 # Fake Schema Trailer
 SCHEMAEND
 
-        MockSchemaDumper.dump(@conn, @stream)
-        assert_equal(expected_output.strip, @stream.to_s)
+      expected_dump_final_output = <<-SCHEMAEND
+  create_sequence "seq_t_item", :increment => 1, :min => false, :max => 2000000, :start => 1, :cache => 5, :cycle => true
+  create_sequence "seq_t_user", :increment => 1, :min => false, :max => 2000000, :start => 1, :cache => 5, :cycle => true
+SCHEMAEND
+
+      MockSchemaDumper.new(@conn, @dump).dump(@stream)
+      assert_equal(expected_stream_output.strip, @stream.to_s)
+      assert_equal(expected_dump_final_output.strip, @dump.final.strip)
       end
     end
   end
