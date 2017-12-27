eea55f614669b2a46f59f75d8f596db7b94f5e2e
Ivan Petroe <petroei@gmail.com>
2017-12-27 10:40:42 +0200

    substituting alias_chain_method with alias_method

--- lib/pg_sequencer/schema_dumper.rb
+++ lib/pg_sequencer/schema_dumper.rb
@@ -22,7 +22,7 @@ module PgSequencer
     extend ActiveSupport::Concern
 
     included do
-      alias_method_chain :tables, :sequences
+      alias_method :tables, :sequences
     end
 
     def tables_with_sequences(stream)
