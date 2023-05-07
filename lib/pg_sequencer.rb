be1028cd56286963e9df08c7f1b13764f27d010f
steakknife <steakknife@users.noreply.github.com>
2023-05-07 12:47:55 -0500

    Update pg_sequencer.rb

--- lib/pg_sequencer.rb
+++ lib/pg_sequencer.rb
@@ -6,11 +6,9 @@ require "pg_sequencer/connection_adapters/postgresql_adapter"
 require "pg_sequencer/sequence_definition"
 require "pg_sequencer/schema_dumper"
 
-require "pg_sequencer/railtie" if defined?(Rails)
-
-begin
-  require "pry"
-rescue LoadError
+if defined?(:Rails)
+  require "pg_sequencer/rollback_support"
+  require "pg_sequencer/railtie"
 end
 
 module PgSequencer
