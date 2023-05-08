662fbe1cd0f36f86b38138797d29e3806908703c
Barry Allard <barry.allard@gmail.com>
2023-05-07 22:15:06 -0500

    fix oops

--- lib/pg_sequencer/railtie.rb
+++ lib/pg_sequencer/railtie.rb
@@ -9,6 +9,7 @@ module PgSequencer
       ActiveSupport.on_load :active_record do
         ActiveRecord::ConnectionAdapters::PostgreSQLAdapter.include(PgSequencer::ConnectionAdapters::PostgresqlAdapter)
         ActiveRecord::SchemaDumper.prepend(PgSequencer::SchemaDumper)
+        ActiveRecord::Migration::CommandRecorder.include(PgSequencer::RollbackSupport)
       end
     end
   end
