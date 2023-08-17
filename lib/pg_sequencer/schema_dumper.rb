ad9c2626e40fdc7676a1ab90ab26b858180eda8f
J. R. Schmid <schmid@infai.org>
2023-08-17 10:18:59 +0200

    no need for this, and not working properly

--- lib/pg_sequencer/schema_dumper.rb
+++ lib/pg_sequencer/schema_dumper.rb
@@ -2,7 +2,6 @@
 # https://github.com/rails/rails/blob/master/activerecord/lib/active_record/schema_dumper.rb
 module PgSequencer
   module SchemaDumper
-
     def tables(stream)
       sequences(stream)
       super(stream)
@@ -19,20 +18,18 @@ module PgSequencer
 
       sequence_statements = unmanaged_sequences.map do |sequence|
         statement_parts = [ ("create_sequence ") + sequence.name.inspect ]
-        statement_parts << ("increment: " + sequence.options[:increment].inspect)
-        statement_parts << ("min: " + sequence.options[:min].inspect)
-        statement_parts << ("max: " + sequence.options[:max].inspect)
-        statement_parts << ("start: " + sequence.options[:start].inspect)
-        statement_parts << ("cache: " + sequence.options[:cache].inspect)
-        statement_parts << ("cycle: " + sequence.options[:cycle].inspect)
-        statement_parts << ("owned_by: " + sequence.options[:owned_by].inspect)
-
+        statement_parts <<  ("increment: " + sequence.options[:increment].inspect)
+        statement_parts <<  ("min: "       + sequence.options[:min].inspect)
+        statement_parts <<  ("max: "       + sequence.options[:max].inspect)
+        statement_parts <<  ("start: "     + sequence.options[:start].inspect)
+        statement_parts <<  ("cache: "     + sequence.options[:cache].inspect)
+        statement_parts <<  ("cycle: "     + sequence.options[:cycle].inspect)
+        # statement_parts << ("owned_by: " + sequence.options[:owned_by].inspect) # FIXME: figure out why sometimes nil?
         "  " + statement_parts.join(", ")
       end
 
       stream.puts sequence_statements.sort.join("\n")
       stream.puts
     end
-
   end
 end
